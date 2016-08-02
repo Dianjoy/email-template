b6e5cf81ba48810156276f2b9ae42be31f1128c7
Meathill <meathill@gmail.com>
2016-08-02 17:55:33 +0800

    换一种方式，直接把 CSS 放到 HTML 里，以便在邮件中保留样式。

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,7 @@
  * Created by meathill on 15/4/2.
  */
 module.exports = function (grunt) {
-  var link_reg = /<link rel="stylesheet" href="(.*)"\s?>/ig
-    , TEMP = 'temp/';
+  var TEMP = 'temp/';
 
   grunt.initConfig({
     clean: {
@@ -16,20 +15,6 @@ module.exports = function (grunt) {
           outputStyle: 'compressed',
           sourcemap: false
         }
-      }
-    },
-    cssmin: {
-      target: {
-        options: {
-          keepSpecialComments: 0
-        },
-        files: [{
-          expand: true,
-          cwd: TEMP,
-          src: [ '*.css', '!*.min.css'],
-          dest: TEMP,
-          ext: '.html.min.css'
-        }]
       }
     },
     htmlmin: {
@@ -49,49 +34,30 @@ module.exports = function (grunt) {
           ext: '.html'
         }]
       }
+    },
+    inlinecss: {
+      main: {
+        options: {},
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['*.html'],
+          dest: TEMP,
+          ext: '.html'
+        }]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-compass');
-
-  grunt.registerTask('extract', 'extract css imports', function () {
-    grunt.file.recurse('src', function (path, root, sub, filename) {
-      var content = grunt.file.read(path)
-        , csses = [];
-      content.replace(link_reg, function (match, css) {
-        if (/^(https?:)?\/\//.test(css)) {
-          return match;
-        }
-        css = grunt.file.read(css.replace('../', ''));
-        csses.push(css);
-      });
-      grunt.file.write(TEMP + filename + '.css', csses.join('\n'));
-    });
-  });
-
-  grunt.registerTask('replace', 'replace imports with css', function () {
-    grunt.file.recurse('src', function (path, root, sub, filename) {
-      var content = grunt.file.read(path)
-        , css = grunt.file.read(TEMP + filename + '.min.css');
-      content = content.replace(link_reg, function (match, src) {
-        if (/^(https?:)?\/\//.test(src)) {
-          return match;
-        }
-        return '<style>' + css + '</style>';
-      });
-      grunt.file.write(TEMP + filename, content);
-    });
-  });
+  grunt.loadNpmTasks('grunt-inline-css');
 
   grunt.registerTask('default', [
     'clean',
     'compass',
-    'extract',
-    'cssmin',
-    'replace',
+    'inlinecss',
     'htmlmin',
     'clean'
   ]);
